903f0a0359c9990251f5f70f91dde19aeaf8e5c3
GabrielYuma <62030680+GabrielYuma@users.noreply.github.com>
2020-12-07 16:41:03 -0300

    a

--- logic.js
+++ logic.js
@@ -1,14 +1,14 @@
 var task = document.querySelector(".list-group-item")
 var submit = document.querySelector(".botao")
+var i = 0;
 
 
 submit.addEventListener('click', function (event) {
     event.preventDefault()
-    
+
     var desc = document.querySelector("#insertDesc").value
     var date = document.querySelector("#insertDate").value
     saveLocStor(desc, date)
-    var i = 1
     console.log(localStorage.getItem('todo'))
 
 })
@@ -19,28 +19,51 @@ function saveLocStor(Desc, Date) {
         Date,
         Desc,
     }
-    if (Desc && Date){
+    if (Desc && Date) {
         if (todo) {
-        let todoObj = JSON.parse(todo)
+            let todoObj = JSON.parse(todo)
             todoObj.push(listar)
 
             let todoString = JSON.stringify(todoObj)
-            
+
             localStorage.setItem('todo', todoString);
-        }else{
+        } else {
             let todoObj = [];
             todoObj.push(listar)
 
             let todoString = JSON.stringify(todoObj)
             localStorage.setItem('todo', todoString);
         }
-    }else{
+        criarTask();
+    } else {
         alert("UM DOS CAMPOS VAZIO")
     }
-    
-
 }
 
+function criarTask() {
+    let array = localStorage.getItem('todo')
+    let listaDeAfazeres = JSON.parse(array)
+
+    let listaSortida = listaDeAfazeres.sort(function(a,b){
+        if(a.Date < b.Date){
+            return -1
+        }
+        if(a.Date > b.Date){
+            return 1
+        }
+        return 0
+    })
+
+    for(i = 0; i < listaSortida.length-1; i++){
+        if(listaSortida[i].Date == listaSortida[i+1].Date){
+            if(){
+                
+            }
+        }
+    }
+
+    console.log(listaSortida)
+}
 
 task.addEventListener('click', function (event) {
     prompt("hello")
